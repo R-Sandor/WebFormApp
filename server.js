fb58a856e545ee791ba75f79ed52696f736cd8ec
Firstname Lastname <your_email@youremail.com>
2019-01-14 21:25:03 -0500

    Down graded version of MongoDB, Saving posts to the database.

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongo = require('mongodb').MongoClient;
+
+var database;
+
 app.use(bodyParser.json());
 
 app.use(function(req, res, next){
@@ -12,6 +16,17 @@ app.use(function(req, res, next){
 app.post('/api/message', function (req, res) {
     console.log(req.body);
     res.status(200);
+  database.collection('messages').insertOne(req.body);
+
+})
+
+mongo.connect("mongodb://localhost:27017/test", function(err, db){
+  if (!err){
+    console.log("we are connected");
+    database = db;
+  }
+  else
+    console.log("Error encountered");
 })
 
 var server = app.listen(5000, function () {
